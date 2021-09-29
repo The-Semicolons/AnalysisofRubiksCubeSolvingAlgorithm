f520e59163193572ebe6177f7ebd35256529f128
Garvit Khurana <500076532@stu.upes.ac.in>
2021-09-29 22:29:46 +0530

    Initialise Files

---
+++ Thistlethwaite's Algorithm/Code/ThistlethwaiteSoln/ThistlethwaiteSoln/rubiks.h
@@ -0,0 +1,7 @@
+#include <vector>
+
+typedef uint_fast32_t FaceArray[6];
+enum eColor{ WHITE, YELLOW, BLUE, GREEN, RED, ORANGE };
+enum eMove{ U = 0, U2 = 1, D = 2, D2 = 3, F = 10, F2 = 11, B = 12, B2 = 13, L = 20, L2 = 21, R = 22, R2 = 23, NONE = 50};
+
+
