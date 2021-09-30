34bcd5de5bf4bef37be029fe34cb5fa3dca41fe1
VanshajGoel <60982919+VanshajGoel@users.noreply.github.com>
2021-09-30 19:35:51 +0530

    Added initialiseSolver() function

--- Thistlethwaite's Algorithm/Code/ThistlethwaiteSoln/ThistlethwaiteSoln/rubiks.cpp
+++ Thistlethwaite's Algorithm/Code/ThistlethwaiteSoln/ThistlethwaiteSoln/rubiks.cpp
@@ -1,1 +1,25 @@
 #include "rubiks.h"
+#include<vector>
+#include<iostream>
+
+
+/// <summary>
+/// Stores solved state of cube and calls function responsible for applying IDDFS algorithm
+/// </summary>
+/// <param name="centers">: Contains color of center face of cube</param>
+void initialiseSolver(const eColor centers[6])
+{
+	FaceArray solvedState;
+	for (int i = 0;i < 6;i++)				//Loop for traversing all the 6 faces of cube
+	{
+		uint_fast32_t face = 0;
+		for (int j = 0;j < 8;j++)			//Nested loop for traversing 8 colors of face of cube
+		{
+			face = (face << 4) | centers[i];  //Storing color of center face in the 8 faces 
+		}
+		solvedState[i] = face;
+	}
+	eMove availableMoves[6] = { L2, R2, F2, B2, U2, D2 };
+	doIDDFS(availableMoves, solvedState, centers, isInitialiseStageGoal);
+	//Make doIDDFS function and isInitialiseStageGoal to continue the code
+}
--- Thistlethwaite's Algorithm/Code/ThistlethwaiteSoln/ThistlethwaiteSoln/rubiks.h
+++ Thistlethwaite's Algorithm/Code/ThistlethwaiteSoln/ThistlethwaiteSoln/rubiks.h
@@ -1,7 +1,17 @@
+#pragma once
 #include <vector>
 
+//Defining an array of block size 32 bits for storing colors of a face of cube
 typedef uint_fast32_t FaceArray[6];
-enum eColor{ WHITE, YELLOW, BLUE, GREEN, RED, ORANGE };
-enum eMove{ U = 0, U2 = 1, D = 2, D2 = 3, F = 10, F2 = 11, B = 12, B2 = 13, L = 20, L2 = 21, R = 22, R2 = 23, NONE = 50};
+
+//Making a enum named eColor for all the colors of cube
+enum eColor { WHITE, YELLOW, BLUE, GREEN, RED, ORANGE };
+
+//Making a enum named eMove for possible moves in the cube
+//two non affecting moves are separated by 2 ie U + 2 -> D
+//all '2' moves are odd. ie U2 is 1, D2 is 3
+//all single moves are even
+//NONE for passing to DFS start
+enum eMove { U = 0, U2 = 1, D = 2, D2 = 3, F = 10, F2 = 11, B = 12, B2 = 13, L = 20, L2 = 21, R = 22, R2 = 23, NONE = 50 };
 
 
